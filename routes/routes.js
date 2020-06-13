d4256eded1ee662a2e67ab5002fb831e9d32aea5
A Rohl <april.rohl2015@gmail.com>
2020-06-13 09:29:59 -0700

    updates to bootstrap css

--- routes/routes.js
+++ routes/routes.js
@@ -8,17 +8,16 @@ var cheerio = require("cheerio");
 var db = require("../models")
 
 module.exports = function (app) {
-    app.get("/scrape", function (req, response) {
+    app.get("/scrape", function (req, res) {
         // First, we grab the body of the html with axios
         axios.get("https://www.buzzfeed.com/").then(function(response) {
             // Then, we load that into cheerio and save it to $ for a shorthand selector
             var $ = cheerio.load(response.data);
-            var count = 0;
+            var scrapedArticles = [];
             // Now, we grab every h2 within an article tag, and do the following:
             $("article h2").each(function (i, element) {
                 // Save an empty result object
                 var result = {};
-                var count = i;
                 // Add the text and href of every link, and save them as properties of the result object
                 result.title = $(this)
                     .children("a")
@@ -26,15 +25,17 @@ module.exports = function (app) {
                 result.link = $(this)
                     .children("a")
                     .attr("href");
-                if (result.title && result.link) {
-                    db.Article.create(result).then(function (dbArticle) {
-                      console.log(dbArticle); 
-                      count++;
-                    }).catch(function (err) {
-                        console.log(err);
-                    });
-                };
+                console.log(result);
+                scrapedArticles.push(result);
+                //     db.Article.create(result).then(function (dbArticle) {
+                //       console.log(dbArticle); 
+                //       count++;
+                //     }).catch(function (err) {
+                //         console.log(err);
+                //     });
+                // };
             });
+            res.render("index", {article:scrapedArticles});
             // response.redirect('/');
         }).catch(function (err) {
             console.log(err);
